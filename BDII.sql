4b6401568b21276bd155122f769e2b0044de53e1
Arthur Carvalho da Silva <arthursilvainformatica@gmail.com>
2024-11-03 13:25:27 -0300

    continuacao 7,8,9,10 e 11

--- BDII.sql
+++ BDII.sql
@@ -29,8 +29,35 @@ FROM passageiro, aeronaves
 WHERE ano == 1996;
 
 
+7.
+SELECT CNPJCompanhia, SUM(Quantidade) AS TotalAeronaves
+FROM HANGARES
+GROUP BY CNPJCompanhia;
 
 
+8.
+SELECT A.Fabricante, A.Modelo, H.CodAeronave, COUNT(DISTINCT H.CNPJCompanhia) AS NumCompanhias
+FROM AERONAVES A
+JOIN HANGARES H ON A.Código = H.CodAeronave AND A.Ano = H.AnoAeronave
+GROUP BY A.Fabricante, A.Modelo, H.CodAeronave
+HAVING COUNT(DISTINCT H.CNPJCompanhia) > 1;
+
+9.
+SELECT Código, Ano, Fabricante, Modelo, País, PreçoTabela
+FROM AERONAVES
+ORDER BY PreçoTabela DESC
+LIMIT 1;
+
+10.
+SELECT CodAeronave, AnoAeronave, Companhia, SUM(Preço) AS TotalPago
+FROM TRANSAÇÕES
+GROUP BY CodAeronave, AnoAeronave, Companhia;
+
+11.
+SELECT Código, Ano, Fabricante, Modelo, País, PreçoTabela
+FROM AERONAVES
+WHERE País IN ('Itália', 'Japão')
+ORDER BY Código ASC, Ano ASC, PreçoTabela DESC;
 
  
 
